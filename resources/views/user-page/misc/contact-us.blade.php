25e5735306794f8e032fdb215b8a476760f59edf
“Garnfil” <jamesgarnfil15@gmail.com>
2023-07-05 16:22:14 +0800

    update padding

--- resources/views/user-page/misc/contact-us.blade.php
+++ resources/views/user-page/misc/contact-us.blade.php
@@ -13,7 +13,7 @@
         </div>
     </div><!-- .page-title -->
     <div class="">
-        <div class="container">
+        <div class="container py-5">
             <div class="row">
                 <div class="col-md-12">
                     <div class="contact-form py-2">
